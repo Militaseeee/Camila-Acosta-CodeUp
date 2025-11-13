a9bde556a0a7d84b9a0c5c0f54c682097bae4f52
Militaseeee <70491463+Militaseeee@users.noreply.github.com>
2025-11-12 23:16:51 -0500

    Fix: merge conflict in UserBasicResponse.java
    Removed merge conflict markers and cleaned up comments.

--- academic_system/src/main/java/com/CAV_RIWI_codeUp/academic_system/dto/user/UserBasicResponse.java
+++ academic_system/src/main/java/com/CAV_RIWI_codeUp/academic_system/dto/user/UserBasicResponse.java
@@ -2,18 +2,10 @@ package com.CAV_RIWI_codeUp.academic_system.dto.user;
 
 import lombok.Data;
 
-<<<<<<< HEAD
-=======
 // It's a DTO to display only the teacher's basic information within the course, without exposing their password
-
->>>>>>> 0dd519c18b0adb86702f98de6222e60d10a59a08
 @Data
 public class UserBasicResponse {
     private Long id_user;
     private String name;
     private String email;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 0dd519c18b0adb86702f98de6222e60d10a59a08
